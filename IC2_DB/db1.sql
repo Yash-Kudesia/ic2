ab088c31f43117a8c5e6e12450218c89874f890f
Yash-Kudesia <yashku2000@gmail.com>
2021-04-14 10:10:03 +0530

    updating DB and rearranging files for my device

--- IC2_DB/db1.sql
+++ IC2_DB/db1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2021 at 05:15 PM
+-- Generation Time: Apr 13, 2021 at 03:55 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
--- server_service_deployer/config.js
+++ server_service_deployer/config.js
@@ -1,27 +1,26 @@
-var color = require('./status_color')
 //---------------------------------------------Databases---------------------------------
-const env = process.env.SYSTEMENV;
+const env = 1;
 
 const NSM_DB_HOST = env ?   "localhost": process.env.npm_config_docDB || "172.18.0.2" 
-const NSM_DB_PASS = "password"
+const NSM_DB_PASS = ""
 const NSM_DB_PORT = 3306
 const NSM_DB_USER = "root"
 const NSM_DB_NAME = "nsm"
 
 const DOCTOR_DB_HOST = env ?   "localhost": process.env.npm_config_docDB || "172.18.0.2" 
-const DOCTOR_DB_PASS = "password"
+const DOCTOR_DB_PASS = ""
 const DOCTOR_DB_PORT = 3306
 const DOCTOR_DB_USER = "root"
 const DOCTOR_DB_NAME = "ic2_doctor"
 
 const AUTH_DB_HOST =  env ?   "localhost": process.env.npm_config_authDB || "172.18.0.2" 
-const AUTH_DB_PASS = "password"
+const AUTH_DB_PASS = ""
 const AUTH_DB_PORT = 3306
 const AUTH_DB_USER = "root"
 const AUTH_DB_NAME = "ic2"
 
 const SERVICE_DB_HOST =  env ?   "localhost": process.env.npm_config_serviceDB || "172.18.0.2" 
-const SERVICE_DB_PASS = "password"
+const SERVICE_DB_PASS = ""
 const SERVICE_DB_PORT = 3306
 const SERVICE_DB_USER = "root"
 const SERVICE_DB_NAME = "db1"
@@ -37,28 +36,28 @@ const AUTH_NAME = "AUTH"
 
 //---------------------------------------------Client Servers---------------------------------
 const C2_PORT = process.env.PORT || 3006;
-const C2_IP = env ?  "localhost": process.env.npm_config_authIP || "172.26.3.171"
-const C2_NAME = "C2"
+const C2_IP = env ?  "localhost": process.env.npm_config_authIP || "172.18.0.11"
+const C2_NAME = "c2"
 
 //---------------------------------------------User Servers---------------------------------
 const W1_PORT = process.env.PORT || 3000;
 const W1_IP = env ?  "localhost": process.env.npm_config_authIP || "172.18.0.6"
-const W1_NAME = "W1"
+const W1_NAME = "w1"
 
 //---------------------------------------------Internal Servers---------------------------------
 const S1_PORT = 3001;
 const S1_IP =  env ?  "localhost": process.env.npm_config_S1IP || "172.18.0.7" 
-const S1_NAME = "S1"
+const S1_NAME = "s1"
 
 const S2_PORT = 3002;
 const S2_IP =  env ?  "localhost": process.env.npm_config_S2IP ||"172.26.3.171"
-const S2_NAME = "S2"
+const S2_NAME = "s2"
 
 const S3_PORT = 3003;
 const S3_IP =  env ?  "localhost": process.env.npm_config_S3IP ||"172.18.0.9"
-const S3_NAME = "S3"
+const S3_NAME = "s3"
 
-console.info(color.FgGreen,"INFO : Server Configuration successfull")
+console.info("INFO : Server Configuration successfull")
 
 module.exports={
     NSM_DB_HOST,
