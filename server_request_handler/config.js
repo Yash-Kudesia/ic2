1dc53300fcbe6c4138d878673e959d75a07b27ed
princepathak <princepathak67@gmail.com>
2021-04-15 19:32:18 +0530

    image creation and makefile generation tested

--- server_request_handler/config.js
+++ server_request_handler/config.js
@@ -1,5 +1,5 @@
 //---------------------------------------------Databases---------------------------------
-const env  = true;
+const env  = false;
 
 const NSM_DB_HOST = env ?   "localhost": process.env.npm_config_docDB || "172.18.0.2" 
 const NSM_DB_PASS = env ? "" :"password"
--- server_service_creator/router.js
+++ server_service_creator/router.js
@@ -3,31 +3,31 @@ const sendRequest = require("./request")
 const { sendMakeFile } = require("./utils")
 var express = require("express");
 var router = express.Router();
-const { createWithConfig } = require("./create_image")
+var { createWithConfig } = require("./create_image_util")
 
 var color = require('./status_color')
 var config = require('./config')
 const S3_IP = config.S3_IP;
 const S3_Port = config.S3_PORT
 
-router.get('/status',(req,res)=>{
+router.get('/status', (req, res) => {
     res.send(200)
 })
 
 router.post("/", (req, res) => {
     //request will be recieved here from S1
-    console.info(color.FgGreen,`INFO : Request received at ${config.S2_NAME}`)
+    console.info(color.FgGreen, `INFO : Request received at ${config.S2_NAME}`)
     var json_req = req.body
     var token = {
         iv: json_req["doctor1"],
         content: json_req["doctor2"]
     }
     doctorAPI(token, json_req["src"], res).then((data) => {
-        if(data=="true"){
-            doctor(config.S2_NAME, config.S3_NAME).then((data)=>{
+        if (data == "true") {
+            doctor(config.S2_NAME, config.S3_NAME).then((data) => {
                 var secret = data
                 var json_req_send = {
-                    username: json_req.user,
+                    username: json_req.username,
                     sessionID: json_req.sessionID,
                     serviceID: json_req.serviceID,
                     physicalID: json_req.physicalID,
@@ -36,42 +36,46 @@ router.post("/", (req, res) => {
                     src: config.S2_NAME
                 }
                 var image_config = {
-                    username: json_req.user,
+                    username: json_req.username,
                     serviceID: json_req.serviceID,
                     os: json_req.os,
                 }
                 //Creating image with the following config
-                var image_status = createWithConfig(image_config)
-                if(image_status=="true"){
-                    console.info(color.FgGreen,"INFO : Image Build  succesfully")
-                }else{
-                    console.info(color.FgGreen,"ERROR : Image build fail")
-                }
-                //-------------------------------------------------------------------------------????
-                //send request to S3
-                //all from S1 - config +MakeFile send by S2 to S3
-                sendRequest(json_req_send, S3_IP, S3_Port, json_req.serviceID, sendMakeFile).then((data)=>{
-                    if(data=="true"){
-                        console.info(color.FgGreen,"INFO : Request executed succesfully")
-                        res.send("true")
-                    }else{
-                        console.info(color.FgGreen,"INFO : Request execution failure")
-                        res.send("false")
+                createWithConfig(image_config).then((data) => {
+                    var image_status = data
+                    if (image_status == "true") {
+                        console.info(color.FgGreen, "INFO : Image Build  succesfully")
+                        //-------------------------------------------------------------------------------????
+                        //send request to S3
+                        //all from S1 - config +MakeFile send by S2 to S3
+                        sendRequest(json_req_send, S3_IP, S3_Port, json_req.serviceID, sendMakeFile).then((data) => {
+                            if (data == "true") {
+                                console.info(color.FgGreen, "INFO : Request executed succesfully")
+                                res.send("true")
+                            } else {
+                                console.info(color.FgGreen, "INFO : Request execution failure")
+                                res.send("false")
+                            }
+                        }).catch((err) => {
+                            console.error(color.FgRed, `ERROR : ${err}`)
+                            console.info(color.FgGreen, "INFO : Request execution failure")
+                            res.send("false")
+                        })
+                    } else {
+                        console.info(color.FgRed, "ERROR : Image build fail")
                     }
-                }).catch((err)=>{
-                    console.error(color.FgRed,`ERROR : ${err}`)
-                    console.info(color.FgGreen,"INFO : Request execution failure")
-                    res.send("false")
                 })
-            }).catch((err)=>{
-                console.error(color.FgRed,`ERROR : ${err}`)
-                console.info(color.FgGreen,"INFO : Request execution failure")
+
+
+            }).catch((err) => {
+                console.error(color.FgRed, `ERROR : ${err}`)
+                console.info(color.FgGreen, "INFO : Request execution failure")
                 res.send("false")
             })
         }
         //sendMakeFile(json_req.serviceID,S3_IP,S3_Port)
     }).catch((err) => {
-        console.error(color.FgRed,`ERROR : ${err}`)
+        console.error(color.FgRed, `ERROR : ${err}`)
         res.send("false")
     })
 
